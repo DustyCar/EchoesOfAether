739696f61dc01194b119cb047721ebd788e1a6bd
Andrew England <30249570@Computing1.local>
2026-01-08 15:03:42 +0000

    end of day commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import image from '../assets/Echoes9.png';
 function Header() {
   return (
     <div>
-      <img src={image} alt="Echoes logo" style={{ width: '100%', minHeight: '320px'}} />
+      <img src={image} alt="Echoes logo" style={{ width: '100%', minHeight: '320px', maxHeight: "400px"}} />
     </div>
   );
 }
